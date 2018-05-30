0c534d07cf09b45c1e5f08443bf907dba16e5d9e
Adrian Trenaman <atrenaman@gilt.com>
2018-05-30 17:45:54 +0100

    Add some comments.

--- src/test/scala/chapter03/ListSpec.scala
+++ src/test/scala/chapter03/ListSpec.scala
@@ -6,6 +6,9 @@ import List._
 class ListSpec extends FlatSpec with Matchers {
 
   "Pattern matching" should "return first matching case" in {
+    // The exercise asks what is the result of the following case statement;
+    // I believe the answer is 3.
+    //
     assertResult(3) {
       List(1,2,3,4,5) match {
         case Cons(x, Cons(2, Cons(4, _))) => x
