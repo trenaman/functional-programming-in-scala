00c1e7319faaed199e065ddf3170735a9579b468
Adrian Trenaman <atrenaman@gilt.com>
2018-06-01 11:56:54 +0100

    Add List.foldRight

--- src/main/scala/chapter03/List.scala
+++ src/main/scala/chapter03/List.scala
@@ -55,4 +55,16 @@ object List {
     case Cons(_, Nil) => Nil
     case Cons(h, t) => Cons(h, init(t))
   }
+
+  def foldRight[A,B](as: List[A], z: B)(f: (A, B) => B): B =
+    as match {
+      case Nil => z
+      case Cons(x, xs) => f(x, foldRight(xs, z)(f))
+    }
+
+  def sum2(ns: List[Int]) =
+    foldRight(ns, 0)(_ + _)
+
+  def product2(ns:List[Int]) =
+    foldRight(ns, 1)(_ * _)
 }
